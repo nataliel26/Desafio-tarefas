2e782b4c8e1c20322d7adec135ed699a6454ad53
nataliel26 <natalielfrechow@gmail.com>
2024-06-26 14:14:54 -0300

    modified:   app.py 	modified:   static/bootstrap.css 	modified:   tasks.db 	modified:   views/index.html

--- app.py
+++ app.py
@@ -7,11 +7,20 @@ app = Bottle()
 def server_static(filepath):
     return static_file(filepath, root='./static')
 
+# Rota para deletar as tarefas
+@app.route('/delete/<id:int>')
+def delete_task(id):
+    task = Task.get(Task.id == id)
+    task.delete_instance()
+    return redirect ('/')
+
+# Rota para exibir a lista de tarefas
 @app.get('/')
 def index():
     tasks = Task.select()
     return template('index', tasks=tasks)
 
+# Rota para adicionar novas tarefas
 @app.post('/add')
 def process_form():
    task_name = request.forms.get('task_name')
@@ -19,13 +28,11 @@ def process_form():
 
    if task_name and task_description:
        Task.create(task_name=task_name, task_description=task_description)
-       return redirect('/')
+       return redirect ('/')
 
 db.connect()
 db.create_tables([Task])
 
-for route in app.routes:
-    print(route.rule)
 
 if __name__ == '__main__':
     run(app, host='localhost', port=8080, debug=True)
